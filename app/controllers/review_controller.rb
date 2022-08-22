0532b0e24c7259fa1724d058ad241fea644fc4ae
Christian TDN <cjwtdn@gmail.com>
2022-08-22 16:12:41 -0600

    added 2 CRUD in review controller

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -5,4 +5,14 @@ get "/reviews" do
     reviews = Review.all
     reviews.to_json
   end
+
+  get "/reviews/:id" do
+    reviews = Review.find(params[:id])
+    reviews.to_json
+  end
+
+  post "/reviews" do
+    reviews = Review.create(score: :score, comment: :comment, user_id: :user_id, movie_id: :movie_id, timestamps: :timestamps)
+  end
+
 end
