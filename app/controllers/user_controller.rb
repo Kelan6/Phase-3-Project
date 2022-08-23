12d6208d150f774b790882fa4f2d2c9f8e34e400
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-08-23 09:42:44 -0600

    added users post

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,4 +5,10 @@ get "/users" do
     users = User.all
     users.to_json
   end
+
+post "/users" do
+  users = User.create(is_critic: params[:is_critic], username: params[:username], password: params[:password])
+  user.tojson
+end
+
 end
